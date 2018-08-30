44d53eab2a6d07952ba9a112c21b8ba3e29bd5d2
Lorenzo Cattaneo <development@lorenzocattaneo.com>
2018-08-30 17:35:02 +0200

    aggiornato esempio

--- example/views/login.php
+++ example/views/login.php
@@ -1,6 +1,6 @@
 <?php
 
-if (!$url = $sp->login("testenv", 0, 1, null, 1, true)) {
+if (!$url = $sp->login("testenv", 0, 1, 1, null, true)) {
     echo "Already logged in !<br>";
     echo "<a href=\"/\">Home</a>";
 } else {
