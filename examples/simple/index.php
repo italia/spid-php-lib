7eb66843073188dc6feb5842ccaea766dcc01cef
Lorenzo Cattaneo <development@lorenzocattaneo.com>
2018-09-07 12:25:11 +0200

    separated simple example and docker one

--- examples/simple/index.php
+++ examples/simple/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once(__DIR__ . "/../vendor/autoload.php");
+require_once(__DIR__ . "/../../vendor/autoload.php");
 if (file_exists(__DIR__ . "/config.php")) require_once(__DIR__ . "/config.php");
 
 ini_set('display_errors', 1);
