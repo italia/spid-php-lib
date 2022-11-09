5d1fd0f3a1fa3ac3ae8ac40495df709c78e56cc8
Simone Bufarini <simone.bufarini@outlook.com>
2022-11-09 14:59:02 +0100

    Removed the example for the db connection

--- example/views/acs.php
+++ example/views/acs.php
@@ -1,14 +1,6 @@
 <?php
-include_once './model/Db.php';
 
 if ($sp->isAuthenticated()) {
-
-    if (isset($settings['database']) && !empty($sp->getResponse())) {
-        $selectedIdp = $_SESSION['idpName'] ?? $_SESSION['spidSession']['idp'];
-        $db = new Db($sp->getIdp($selectedIdp));
-        $db->updateLogWithResponseData($sp->getResponse());
-    }
-
     foreach ($sp->getAttributes() as $key => $attr) {
         echo $key . ' - ' . $attr . '<br>';
     }
--- example/views/login_post.php
+++ example/views/login_post.php
@@ -1,20 +1,8 @@
 <?php
-include_once './model/Db.php';
 
-if (isset($_POST) && isset($_POST['selected_idp'])) {
-    $idp = $_POST['selected_idp'];
-}
-if (isset($_GET) && isset($_GET['selected_idp'])) {
-    $idp = $_GET['selected_idp'];
-}
-
-if (!$url = $sp->loginPost($idp ?? "testenv", 0, 1, 1, null, true)) {
+if (!$url = $sp->loginPost("testenv", 0, 1, 1, null, true)) {
     echo "Already logged in !<br>";
     echo "<a href=\"/\">Home</a>";
 } else {
-    if (isset($settings['database'])) {
-		$db = new Db($sp->getIdp($idp));
-		$db->insertAuthnDataIntoLog();
-	}
     echo $url;
 }
